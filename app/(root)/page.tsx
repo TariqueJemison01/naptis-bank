ada2a60771f639553de20f701277edec38024b21
Tarique Jemison <tjemison@my.centennialcollege.ca>
2024-08-06 18:21:06 -0400

    update dependencies

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,27 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  return <div>HOME</div>;
+  const LoggedIn = { firstName: "Tarique" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome to Naptis Bank"
+            user={LoggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 export default Home;
